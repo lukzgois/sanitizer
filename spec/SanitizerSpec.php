80166341cc44f3d6fa73cd85dd39c7b89264d5e2
Lukz <lucaspgois@gmail.com>
2015-05-12 16:21:38 -0300

    Initial Commit

---
+++ spec/SanitizerSpec.php
@@ -0,0 +1,136 @@
+<?php namespace spec\Lukzgois\Sanitizer;
+
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class SanitizerSpec extends ObjectBehavior
+{
+
+	public function let()
+	{
+		$this->beAnInstanceOf('spec\Lukzgois\Sanitizer\TestSanitizer');
+	}
+
+	public function it_sanitizes_data_against_a_set_of_rules()
+	{
+		$this->sanitize(
+			['slug' => 'SOME-SLUG'],
+			['slug' => 'strtolower']
+		)->shouldReturn(['slug' => 'some-slug']);
+
+		$this->sanitize(
+			['first' => 'john'],
+			['first' => 'ucwords', 'last' => 'ucwords']
+		)->shouldReturn(['first' => 'John']);
+	}
+
+	public function it_can_apply_multiple_sanitizers()
+	{
+		$this->sanitize(
+			['name' => '   john doe   '],
+			['name' => 'trim|ucwords']
+		)->shouldReturn(['name' => 'John Doe']);
+	}
+
+	public function it_allows_sanitizers_to_optionally_be_an_array()
+	{
+		$this->sanitize(
+			['name' => '  john doe  '],
+			['name' => ['trim', 'ucwords']]
+		)->shouldReturn(['name' => 'John Doe']);
+	}
+
+	public function it_fetches_rules_off_a_subclass_if_they_are_not_passed_in()
+	{
+		$this->sanitize(['name' => '   john'])->shouldReturn(['name' => 'John']);
+	}
+
+	public function it_allows_for_custom_sanitization()
+	{
+		$this->sanitize(['phone' => '555-555-5555'])->shouldReturn(['phone' => '5555555555']);
+	}
+
+	public function	 it_allows_for_custom_class_sanitization()
+	{
+		$this->sanitize(
+			['phone' => '  555-555-5555'],
+			['phone' => 'spec\Lukzgois\Sanitizer\CustomSanitizer']
+		)->shouldReturn(['phone' => '5555555555']);
+
+		$this->sanitize(
+			['phone' => '  555-555-5555'],
+			['phone' => 'spec\Lukzgois\Sanitizer\CustomSanitizer@otherSanitization']
+		)->shouldReturn(['phone' => '  5555555555']);
+	}
+
+	public function it_allows_the_custom_class_optionally_have_options()
+	{
+		$this->sanitize(
+			['number' => '123456'],
+			['number' => 'spec\Lukzgois\Sanitizer\OptionsSanitizer:5']
+		)->shouldReturn(['number' => '12345']);
+
+		$this->sanitize(
+			['number' => '123456'],
+			['number' => 'spec\Lukzgois\Sanitizer\OptionsSanitizer@custom:2,3']
+		)->shouldReturn(['number' => '345']);
+	}
+
+	public function it_set_a_default_value_for_a_field()
+	{
+		$this->sanitize(
+			['name' => null],
+			['name' => 'default:test']
+		)->shouldReturn(['name' => 'test']);
+
+		$this->sanitize(
+			['name' => '123'],
+			['name' => 'default:test']
+		)->shouldReturn(['name' => '123']);
+
+	}
+}
+
+
+class TestSanitizer extends \Lukzgois\Sanitizer\Sanitizer {
+
+	public function rules()
+	{
+		return [
+			'name' => 'trim|ucwords',
+			'phone' => 'phone'
+		];
+	}
+
+	public function sanitizePhone($value)
+	{
+		return str_replace('-', '', $value);
+	}
+}
+
+class CustomSanitizer {
+
+	public function sanitize($value)
+	{
+		return trim(str_replace('-', '', $value));
+	}
+
+	public function otherSanitization($value)
+	{
+		return str_replace('-', '', $value);
+	}
+}
+
+class OptionsSanitizer {
+
+	public function sanitize($value, $max)
+	{
+		return substr($value, 0, $max);
+	}
+
+	public function custom($value, $min, $max)
+	{
+		return substr($value, $min, $max);
+	}
+
+}
---
+++ src/Sanitizer.php
@@ -0,0 +1,118 @@
+<?php namespace Lukzgois\Sanitizer;
+
+/**
+ * Class Sanitizer
+ * @package App\Sanitizer
+ */
+abstract class Sanitizer {
+
+
+	/**
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [];
+	}
+
+	/**
+	 * @param $data
+	 * @param $rules
+	 * @return mixed
+	 */
+	public function sanitize($data, $rules = null)
+	{
+		$rules = $rules ?: $this->rules();
+		foreach ($rules as $field => $sanitizers)
+		{
+			//if (!isset($data[$field])) continue;
+
+			$data = $this->applySanitizers($data, $sanitizers, $field);
+		}
+
+		return $data;
+	}
+
+	/**
+	 * @param $sanitizers
+	 * @return array
+	 */
+	private function splitSanitizers($sanitizers)
+	{
+		return is_array($sanitizers) ? $sanitizers : explode('|', $sanitizers);
+	}
+
+	/**
+	 * @param $data
+	 * @param $sanitizers
+	 * @param $field
+	 * @return mixed
+	 */
+	private function applySanitizers($data, $sanitizers, $field)
+	{
+		foreach ($this->splitSanitizers($sanitizers) as $sanitizer)
+		{
+			if (strpos($sanitizer, 'default:') !== false)
+			{
+				$data[$field] = $this->applyDefaultValue($data[$field], $sanitizer);
+				continue;
+			}
+
+			if (isset($data[$field]))
+				$data[$field] = $this->applySanitizerTo($data[$field], $sanitizer);
+		}
+
+		return $data;
+	}
+
+	/**
+	 * @param $value
+	 * @param $sanitizer
+	 * @return mixed
+	 */
+	private function applySanitizerTo($value, $sanitizer)
+	{
+		$options = [$value];
+
+		// verify if the rule has options
+		$sanitizer = explode(':', $sanitizer);
+		if (isset($sanitizer[1]))
+			$options = array_merge($options, explode(',', $sanitizer[1]));
+
+		// verify if the rule has a custom method
+		$sanitizer = explode('@', $sanitizer[0]);
+		if (isset($sanitizer[1]))
+			$method = $sanitizer[1];
+
+
+		// If the sanitizer method is an existent class,
+		// the let's use it
+		if (class_exists($sanitizer[0]))
+		{
+			$class = new $sanitizer[0];
+
+			// If a custom method is specified (classname@customMethod)
+			// then let's use it, otherwise we use the sanitize method
+			return isset($method)
+				? call_user_func_array([$class, $method], $options)
+				: call_user_func_array([$class, 'sanitize'], $options);
+		}
+
+		// If a custom sanitizer is registered in the subclass,
+		// then let's trigger that instead.
+		// If not, so we'll use a normal function
+		$method = 'sanitize' . ucwords($sanitizer[0]);
+		$value = method_exists($this, $method)
+			? call_user_func([$this, $method], $value)
+			: call_user_func($sanitizer[0], $value);
+
+		return $value;
+	}
+
+	private function applyDefaultValue($value, $sanitizer)
+	{
+		return (isset($value))
+			? $value
+			: explode(':', $sanitizer)[1];
+	}
+}
